f865290375f932104bc1ed93a99f317610bfd41a
George Garcia <georgegarcia05012001@gmail.com>
2025-08-12 09:48:06 -0500

    added feature where login data is unique for each individual user

--- GameAchievementTracker/Controllers/HomeController.cs
+++ GameAchievementTracker/Controllers/HomeController.cs
@@ -2,26 +2,42 @@ using Microsoft.AspNetCore.Mvc;
 using GameAchievementTracker.Models;
 using GameAchievementTracker.Data;
 using System.Linq;
+using System.Security.Claims;
+
 
 namespace GameAchievementTracker.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _context;
+        
 
         public HomeController(ApplicationDbContext context)
         {
             _context = context;
+           
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var games = _context.Games
-                .Where(g => g.Id == null)
+            if (!User.Identity.IsAuthenticated)
+            {
+                return View(new List<Game>());
+            }
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userId))
+            {
+                return View(new List<Game>());
+            }
+
+            var userGames = _context.Games
+                .Where(g => g.UserId == userId)
                 .OrderByDescending(g => g.Id)
                 .Take(3)
                 .ToList();
-            return View(games);
+
+            return View(userGames);
         }
     }
 }
